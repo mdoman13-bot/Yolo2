3edc97a7c8d77a7e418dc36ddd9452aa3fa1df84
Michael <mbdoman@iu.edu>
2024-07-09 11:08:51 -0400

    added yolov8s-worldv2 model

--- trackinglivev9.py
+++ trackinglivev9.py
@@ -2,13 +2,13 @@ import cv2
 from ultralytics import YOLO
 
 # Initialize a YOLO-World model
-model = YOLO('models/yolov8s-world.pt')  # or choose yolov8m/l-world.pt
+model = YOLO('models/yolov8s-worldv2.pt')  # or choose yolov8m/l-world.pt
 
 # model.to('mps')
 # Define custom classes
-model.set_classes(["car", "bus", "building", "traffic light"])
+model.set_classes(["car", "bus", "building", "traffic light", "person"])
 
-results = model.track(source="media/newvid2.mp4", conf=0.3, iou=0.5, show=True)
+results = model.track(source="media/drone_cars.mp4", conf=0.3, iou=0.5, show=True)
 
 # Add functionality to end the program when 'q' is pressed
 while True:
