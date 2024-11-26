5d817222c2330aceb1e524fc8bbbba1e250a8bbe
Modman <mbdoman@me.com>
2024-11-26 10:20:58 -0500

    Update YOLO model and enhance image processing with custom classes and annotations

--- image.py
+++ image.py
@@ -1,13 +1,35 @@
+import cv2
+import numpy as np
+import supervision as sv
 from ultralytics import YOLO
 
-# Initialize a YOLO-World model
-model = YOLO("models/yolov8s-world.pt")  # or choose yolov8m/l-world.pt
-
+# Load the YOLO model
+model = YOLO("models/yolov8s-worldv2.pt")
+# model.to('mps')
 # Define custom classes
-model.set_classes(["person", "pool", "water"])
+model.set_classes(["pool", "car", "person", "road", "tree", "building", "sky"])
 
-# Execute prediction for specified categories on an image
-results = model.predict("media/beach2.jpg")
+# Define the callback function for processing
+def callback(image_slice: np.ndarray) -> sv.Detections:
+    result = model(image_slice)[0]
+    return sv.Detections.from_ultralytics(result)
 
-# Show results
-results[0].show()
+# Initialize the annotators
+bounding_box_annotator = sv.BoundingBoxAnnotator()
+label_annotator = sv.LabelAnnotator()
+
+# Read the image
+image = cv2.imread("media/pool1.png")
+
+# Process the image
+detections = sv.InferenceSlicer(callback=callback)(image)
+annotated_image = bounding_box_annotator.annotate(
+    scene=image, detections=detections)
+annotated_image = label_annotator.annotate(
+    scene=annotated_image, detections=detections)
+
+# Save or display the annotated image
+cv2.imwrite("media/annotated_pool1.png", annotated_image)
+cv2.imshow("Annotated Image", annotated_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
