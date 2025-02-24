2653356e0f4ec566177369f29395a3b857eff2a7
Michael <mdoman13-bot@users.noreply.github.com>
2025-02-24 07:21:06 -0500

    Update YOLO to V12 in model path and change device setting to MPS for inference, speedup of 8x

--- new3.py
+++ new3.py
@@ -5,8 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load the YOLO model
-model = YOLO('models/yolo11x')  # Replace with the path to your model
-model.to('cuda') if torch.cuda.is_available() else model.to('cpu') # Use GPU for inference
+model = YOLO('models/yolo12m')  # Replace with the path to your model
+model.to('cuda') if torch.cuda.is_available() else model.to('mps') # Use GPU for inference
 # To get this to work, I had to pip uninstall opencv-python then pip install opencv-python
 # Open the live stream
 # keystone -> 31
