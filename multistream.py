5f3875576cfbf1ea4a4ed74ffd4294d54377d5d9
Modman <mbdoman@me.com>
2025-06-17 07:46:23 -0400

    Update YOLO model path from yolo11x to yolo11n and adjust device handling

--- multistream.py
+++ multistream.py
@@ -7,8 +7,8 @@ import threading
 import math
 
 # Load the YOLO model
-model = YOLO('models/yolo11x')  # Replace with the path to your model
-model.to('cuda') if torch.cuda.is_available() else model.to('mps') # Use GPU for inference
+model = YOLO('models/yolo11n')  # Replace with the path to your model
+model.to('cuda') if torch.cuda.is_available() else model.to('cpu') # Use GPU for inference
 # To get this to work, I had to pip uninstall opencv-python then pip install opencv-python
 # Open the live stream
 # keystone -> 31
